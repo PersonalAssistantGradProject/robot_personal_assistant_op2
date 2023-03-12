254b6247aa72c29ac4a97494d1465dbb5b9c3aff
ahmadimnajjar <amapineapple@gmail.com>
2023-03-12 19:32:36 +0300

    new commit

--- scripts/Text to speech/tts.py
+++ scripts/Text to speech/tts.py
@@ -1,1 +1,17 @@
 import pyttsx3
+
+
+engine = pyttsx3.init()
+
+
+
+# A function to allow the robot to speech any text it's given
+def Say(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+
+# this python script will subscribe to a ROS topic that has string messages (text) and then convert it to speech.
+
+
