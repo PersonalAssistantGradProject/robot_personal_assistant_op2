4da1ed529c10cb47eb4bf981430eb2f317247c8f
ahmadimnajjar <amapineapple@gmail.com>
2023-03-16 18:19:44 +0300

    tts improvement

--- scripts/Text to speech/text_processing.py
+++ scripts/Text to speech/text_processing.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+
 import speech_recognition as sr       # PART 2
-import requests                       # PART 3
-from bs4 import BeautifulSoup         # PART 3
+import requests                       # PART 4
+from bs4 import BeautifulSoup         # PART 4
+import random                         # PART 4
 
-
+import rospy                          # PART 3
+from std_msgs.msg import String       # PART 3
 
 
 
@@ -13,7 +17,7 @@ from bs4 import BeautifulSoup         # PART 3
 
 
 # Initialize recognizer class (for recognizing the speech)
-""" r = sr.Recognizer()
+r = sr.Recognizer()
 
 
 while True:
@@ -27,28 +31,81 @@ while True:
 
         # using google speech recognition
         #print("Text: "+r.recognize_google(audio_text))
-        text = r.recognize_google(audio_text)
-        print ("User said: ", text)
+        user_said = r.recognize_google(audio_text)
+        print ("User said: ", user_said)
 
-        if (text.find("hey Darwin") != -1 or text.find("hey darling") != -1):
+        if (user_said.find("hey Darwin") != -1 or user_said.find("hey darling") != -1):
 
             print("how can i help you?") # placeholder
             with sr.Microphone() as source:
 
                 audio_text = r.listen(source)
                 #print("Text: "+r.recognize_google(audio_text))
-                text = r.recognize_google(audio_text)
-                print ("User said: ", text)
-                break """
+                user_said = r.recognize_google(audio_text)
+                print ("User said: ", user_said)
+                break
+
+
+####################################################### PART 3 - function to publish the text to the robot
+
+pub = rospy.Publisher('tts',String,queue_size=10)
+rospy.init_node('text_pub',anonymous=True)
+rate = rospy.Rate(10)
+def speak_text(output_text):
+
+    rospy.loginfo(output_text)
+    pub.publish(output_text)
+    rate.sleep()
+
+####################################################### PART 4 - generate the required text
+
+
+output_text = ""
+
+
+# telling a joke
+if (user_said.find("joke") != -1):
+
+    joke_num = random.randint(0, 1)
+    if (joke_num == 0):
+        output_text = "Why don\'t scientists trust atoms? Because they make up everything. hahahahaha"
+
+    elif (joke_num == 1):
+        output_text = "What did the policeman say to his hungry stomach? Freeze. You\'re under a vest. hahahahahaha"
+
+speak_text(output_text)
+
+
+
+# take a note
+if (user_said.find("note") != -1):
+
+
+    with sr.Microphone() as source:
+
+        print("Say your note!")
+        output_text = "Please speak out your note!"
+        speak_text(output_text)
+
+        audio_text = r.listen(source)
+
+        # using google speech recognition
+        #print("Text: "+r.recognize_google(audio_text))
+        user_said = r.recognize_google(audio_text)
+        print ("User said: ", user_said)
+        # Open file in write mode
+        with open("example.txt", "w") as f:
+            # Write string to file
+            f.write(user_said)
+            output_text = "Your note was saved as a txt file!"
+            speak_text(output_text)
 
 
 
 
-####################################################### PART 3 - generate the required text
 
-
-
-query = "who won world cup 2022"
+# google api
+""" query = "who won world cup 2022"
 url = f"https://www.google.com/search?q={query}"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 response = requests.get(url, headers=headers)
@@ -58,6 +115,7 @@ print (response)
 soup = BeautifulSoup(response.content, 'html.parser')
 print (soup)
 answer_box = soup.find('div', class_='Z0LcW')
-print(answer_box)
+print(answer_box) """
 
-####################################################### PART 4 - publish the text to the robot
+
+
--- scripts/Text to speech/tts.py
+++ scripts/Text to speech/tts.py
@@ -1,8 +1,15 @@
-import pyttsx3
+#!/usr/bin/env python
 
+import pyttsx3
+import rospy
+from std_msgs.msg import String
 
 engine = pyttsx3.init()
 
+
+
+def callback(data):
+    Say(data.data)
 
 
 # A function to allow the robot to speech any text it's given
@@ -10,6 +17,24 @@ def Say(text):
     engine.say(text)
     engine.runAndWait()
 
+
+# spin() simply keeps python from exiting until this node is stopped
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('text_sub', anonymous=True)
+
+    rospy.Subscriber('tts', String, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
 
 
 # this python script will subscribe to a ROS topic that has string messages (text) and then convert it to speech.
