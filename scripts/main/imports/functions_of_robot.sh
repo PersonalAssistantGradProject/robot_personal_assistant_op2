f4cdbe11721a53019168a3a27cc958b86ec16fe5
ahmadimnajjar <amapineapple@gmail.com>
2023-05-24 16:46:17 +0300

    launch files test 4

--- scripts/main/imports/functions_of_robot.sh
+++ scripts/main/imports/functions_of_robot.sh
@@ -1,6 +1,7 @@
-
+#!/bin/bash
 cd
 cd catkin_ws/src/robot_personal_assistant_op2/scripts/main/
+python image_publisher.py
 python3.9 audio_sender.py &
 python3.9 audio_note_player.py &
 python3.9 text_to_speech_subscriber.py &
